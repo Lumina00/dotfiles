f4bbd230ff7fcb5e7ceafbe825a3a62582599049
lumina00 <lumina.static@gmail.com>
2020-11-29 17:35:34 +0900

    update

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 sudo pacman -S --no-confirm reflector 
 sudo reflector -f 30 -l 30 --protocol https --protocol http --sort rate --country Japan --verbose --save /etc/pacman.d/mirrorlist
-sudo pacman -Syu --no-confirm rustup ruby zsh uim refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amd-vlk btrfs-progs easytag git yarn 
+sudo pacman -Sy --no-confirm rustup ruby zsh uim refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amd-vlk btrfs-progs easytag git yarn 
 rustup install stable
 cargo install bottom exa dust rua cargo-update
 
