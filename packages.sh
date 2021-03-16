0a4cd16ceeda743730fa7367ccf472908388a26f
lumina00 <lumina.static@gmail.com>
2021-03-16 17:34:15 +0900

    update

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 sudo pacman -S --no-confirm reflector 
 sudo reflector -f 30 -l 30 --protocol https --protocol http --sort rate --country Japan --verbose --save /etc/pacman.d/mirrorlist
-sudo pacman -Sy --no-confirm rustup ruby zsh uim refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amd-vlk btrfs-progs easytag git yarn 
+sudo pacman -Sy --no-confirm rustup ruby zsh uim refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amd-vlk btrfs-progs easytag yarn anthy lvm2
 rustup install stable
 cargo install bottom exa dust rua cargo-update starship
 
@@ -13,4 +13,4 @@ cp -r .config ~/
 cp -b .zshrc ~/
 sudo cp -i nftables.conf /etc/nftables.conf
 
-rua install dislocker dcron 
+rua install dislocker dcron aic94xx-firmware xxd-standalone
