b6ea5c776f22f82ffe4d5d06bf2e59bf19b219d4
lumina00 <lumina.static@gmail.com>
2021-06-03 07:24:33 +0900

    update

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 sudo pacman -S --noconfirm reflector 
 sudo reflector -f 30 -l 30 --protocol https --protocol http --sort rate --country Japan --verbose --save /etc/pacman.d/mirrorlist
-sudo pacman -Sy --noconfirm rustup ruby zsh uim refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amdvlk btrfs-progs easytag yarn anthy lvm2 pipewire pipewire-pulse 
+sudo pacman -Sy --noconfirm rustup ruby zsh uim refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs qt5-imageformats amd-ucode p7zip amdvlk btrfs-progs easytag yarn anthy lvm2 pipewire pipewire-pulse 
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
