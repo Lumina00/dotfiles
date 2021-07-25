d310b28e0d9afe1999e7279695c8472db86268d1
lumina00 <lumina.static@gmail.com>
2021-07-25 11:08:13 +0900

    update

--- packages.sh
+++ packages.sh
@@ -8,6 +8,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir ~/.zinit 
 git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
+cp .tmux.conf ~/
 cp -r .config ~/
 cp -b .zshrc ~/
 sudo cp nftables.conf /etc/nftables.conf
