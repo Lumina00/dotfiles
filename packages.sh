33a4283203cc27690e7adda2a278d1fbaad0214e
lumina00 <lumina.static@gmail.com>
2021-09-04 13:46:56 +0900

    update

--- packages.sh
+++ packages.sh
@@ -1,19 +1,17 @@
 #!/bin/sh 
 sudo pacman -S --noconfirm reflector 
 sudo reflector -f 30 -l 30 --protocol https --protocol http --sort rate --country Japan --verbose --save /etc/pacman.d/mirrorlist
-sudo pacman -Sy --noconfirm rustup ruby zsh refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amdvlk btrfs-progs easytag yarn anthy lvm2 pipewire pipewire-pulse 
-
-sudo pacman -S --noconfirm --needed gst-plugins-good qt5-imageformats
+sudo pacman -Sy --noconfirm --needed rustup ruby zsh refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amdvlk btrfs-progs easytag yarn anthy lvm2 pipewire pipewire-pulse gst-plugins-good qt5-imageformats
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir ~/.zinit 
 git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
 cp .tmux.conf ~/
 cp -r .config ~/
-cp -b .zshrc ~/
-cp -b .gdbinit ~/
-sudo cp -b config.ini /etc/ly/
+cp -r .zshrc ~/
+cp -r .gdbinit ~/
+sudo cp -r config.ini /etc/ly/
 sudo cp nftables.conf /etc/nftables.conf
 rustup install stable
-cargo install bottom exa dust rua cargo-update starship
+cargo install bottom exa du-dust rua cargo-update starship
 rua install dislocker dcron aic94xx-firmware xxd-standalone
