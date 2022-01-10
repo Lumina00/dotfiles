4e9817105b1d94e4e9ff809c2eae2be74c0b40ef
lumina00 <lumina.static@gmail.com>
2022-01-10 15:50:33 +0900

    add xdg-user-dirs

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 sudo pacman -S --needed --noconfirm reflector 
 sudo reflector -f 30 -l 30 --protocol https --protocol http --sort rate --country Japan --verbose --save /etc/pacman.d/mirrorlist
-sudo pacman -S --noconfirm --needed rustup ruby zsh refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amdvlk btrfs-progs easytag yarn anthy lvm2 pipewire pipewire-pulse gst-plugins-good qt5-imageformats mesa-vdpau vulkan-radeon plasma-destkop powerdevil bluedevil plasma-pa plasma-nm spectacle
+sudo pacman -S --noconfirm --needed rustup ruby zsh refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amdvlk btrfs-progs easytag yarn anthy lvm2 pipewire pipewire-pulse gst-plugins-good qt5-imageformats mesa-vdpau vulkan-radeon plasma-destkop powerdevil bluedevil plasma-pa plasma-nm spectacle xdg-user-dirs
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir ~/.zinit 
@@ -16,3 +16,6 @@ cargo install bottom du-dust paru cargo-update
 ./paru.sh
 sudo \cp -f config.ini /etc/ly/
 ./privacy.sh
+xdg-user-dirs-update
+
+exit
