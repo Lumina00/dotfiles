53cbbcefd8b2efab1b6084d4f242801e98b85081
lumina00 <lumina.static@gmail.com>
2022-05-07 20:33:51 +0900

    Added CCLS

--- shell/packages.sh
+++ shell/packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 sudo pacman -S --needed --noconfirm reflector 
 sudo reflector -f 30 -l 30 --protocol https --protocol http --sort rate --country Japan --verbose --save /etc/pacman.d/mirrorlist
-sudo pacman -S --noconfirm --needed rustup ruby zsh refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amdvlk btrfs-progs easytag yarn anthy lvm2 pipewire pipewire-pulse gst-plugins-good qt5-imageformats mesa-vdpau vulkan-radeon plasma-destkop powerdevil bluedevil plasma-pa plasma-nm spectacle kscreen xdg-user-dirs
+sudo pacman -S --noconfirm --needed rustup ruby zsh refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker nomacs amd-ucode p7zip amdvlk btrfs-progs easytag yarn anthy lvm2 pipewire pipewire-pulse gst-plugins-good qt5-imageformats mesa-vdpau vulkan-radeon plasma-destkop powerdevil bluedevil plasma-pa plasma-nm spectacle kscreen xdg-user-dirs ccls
 ./rust.sh
 cargo install bottom du-dust paru cargo-update
 ./paru.sh
