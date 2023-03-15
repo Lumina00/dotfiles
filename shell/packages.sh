8f7d32e799a05f6beb9bc1787def3dbdd650740e
lumina00 <lumina.static@gmail.com>
2023-03-16 03:44:23 +0900

    update

--- shell/packages.sh
+++ shell/packages.sh
@@ -1,4 +1,5 @@
 #!/bin/sh 
+sudo sed -i "37s/#ParallelDownloads/ParallelDownloads/" /etc/pacman.conf
 sudo pacman -S --needed --noconfirm reflector 
 sudo reflector -f 30 -l 30 --protocol https --protocol http --sort rate --country Japan --verbose --save /etc/pacman.d/mirrorlist
 sudo pacman -S --noconfirm --needed rustup zsh refind bandwhich mpv lollypop firefox qbittorrent neovim flatpak docker \
