995e3faf5a4892c67f786028c1b01331306df337
lumina00 <lumina.static@gmail.com>
2023-05-18 14:22:21 +0900

    update

--- shell/pacman.sh
+++ shell/pacman.sh
@@ -1,8 +1,11 @@
 #!/bin/sh 
-sudo sed -i "37s/#ParallelDownloads/ParallelDownloads/" /etc/pacman.conf
-sudo pacman -S --needed --noconfirm reflector 
-sudo reflector -f 30 -l 30 --protocol https --protocol http --sort rate --country Japan --verbose --save /etc/pacman.d/mirrorlist
-sudo pacman -S --noconfirm --needed rustup zsh refind bandwhich mpv lollypop firefox-developer-edition qbittorrent neovim flatpak docker amd-ucode p7zip amdvlk btrfs-progs easytag yarn anthy lvm2 pipewire pipewire-pulse gst-plugins-good qt5-imageformats mesa-vdpau vulkan-radeon libva-mesa-driver plasma-desktop powerdevil bluedevil plasma-pa plasma-nm spectacle kscreen xdg-user-dirs xclip kdeconnect konsole dolphin xorg-server 
+#sudo sed -i "37s/#ParallelDownloads/ParallelDownloads/" /etc/pacman.conf
+#sudo pacman -S --needed --noconfirm reflector 
+#sudo reflector -f 30 -l 30 --protocol https --protocol http --sort rate --country Japan --verbose --save /etc/pacman.d/mirrorlist
+sudo pacman -S --noconfirm --needed rustup cmake docker \
+  zsh refind bandwhich mpv lollypop gst-plugins-good easytag firefox-developer-edition firefox-developer-edition-i18n-ja qbittorrent neovim xclip rsync\
+  flatpak amd-ucode p7zip btrfs-progs pipewire pipewire-pulse wireplumber amdvlk mesa-vdpau vulkan-radeon libva-mesa-driver xorg-{server,xauth,xinit} xf86-video-amdgpu xdg-user-dirs nftables noto-fonts-cjk\
+  plasma-desktop powerdevil bluedevil plasma-pa plasma-nm spectacle kscreen qt5-imageformats kdeconnect konsole dolphin
 #bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
 exit
--- shell/setting.sh
+++ shell/setting.sh
@@ -6,12 +6,14 @@ cp .tmux.conf ~/
 cp -r .config ~/        
 \cp -f .zshrc ~    
 \cp -f .gdbinit ~    
+mkdir -p ~/.var/app
 sudo \cp -f nftables.conf /etc/nftables.conf    
 sudo cp nft-blackhole.conf /etc/nft-blackhole.conf
 sudo cp linux-lts.preset /etc/mkinitcpio.d/linux-lts.preset
 sudo cp cmdline /etc/kernel/cmdline 
 sudo cp mkinitcpio.conf /etc/mkinitcpio.conf
-sudo \cp -f config.ini /etc/ly/    
+sudo \cp -f config.ini /etc/ly/
+sudo ln -s ~/.var/app /var/lib/flatpak/app/
 ../shell/privacy.sh
 xdg-user-dirs-update    
 sudo systemctl mask NetworkManager-wait-online.service   
